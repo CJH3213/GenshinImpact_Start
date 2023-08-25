af38eaf102e6ce0433d35a7c98833e0c41ec3599
CJH3213 <1603571877@qq.com>
2023-08-25 16:09:41 +0800

    修正一些文本名称

--- Genshin_Impact_Start/Genshin_Impact_Start/Genshin_Impact_Start/Form1.Designer.cs
+++ Genshin_Impact_Start/Genshin_Impact_Start/Genshin_Impact_Start/Form1.Designer.cs
@@ -151,7 +151,7 @@
             // notifyIcon1
             // 
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "检测白屏中…";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
